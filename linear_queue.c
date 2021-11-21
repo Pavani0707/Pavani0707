6456684df5ed5f90965dcde6b01065b67bb96032
Pavani0707 <93459294+Pavani0707@users.noreply.github.com>
2021-11-21 12:12:16 +0530

    linear queue implementation
    C program to implement linear queue using array.

--- linear_queue.c
+++ linear_queue.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
-#include <stdlib.h>
-int cir_queue[20], size, front = -1, rear =-1;
+int queue[20], n, front = -1, rear =-1;
 void enqueue();
 void dequeue();
 void display();
 int main()
 {
 	int choice;
-	printf("Enter the size of circular queue: ");
-	scanf("%d", &size);
+	printf("Enter the size of queue: ");
+	scanf("%d", &n);
 	printf("\n operations:\n1.enqueue/insertion\n2.dequeue/deletion\n3.display\n4.exit\n");
-	while(1)
+	do
 	{
 		printf("\nEnter the choice : ");
 		scanf("%d", &choice);
@@ -33,50 +32,46 @@ int main()
 				}
 			case 4:
 				break;
-				exit(1);
 			default:
 				printf("\nEnter valid number!!\n");
 		}
-	}
+	}while(choice!=4);
 	return 0;
 }
 
 void enqueue()
 {
 	int num;
-	if(front == (rear +1)%size)
-	{
-		printf("\nqueue overflow");
-		return;
-    }
-    else if(front ==-1)
+	if(rear== n -1)
+	    printf("\nqueue overflow");
+    else
     {
-    	front = 0;
-    	rear = 0;
+    	if(front ==-1)
+    	    front =0;
+    	printf("Enter the value to enqueue : ");
+    	scanf("%d", &num);
+    	rear++;
+    	queue[rear]=num;
 	}
-	else
-	    rear =(rear +1)%size;
-	scanf("%d", &num);
-	cir_queue[rear] = num;
 }
 
 void dequeue()
 {
-	int num;
 	if(front == -1)
 	{
-		printf("queue is empty\n");
+		printf("queue underflow\n");
+		return;
 	}
 	else if(front == rear)
 	{
+		printf("Element deleted is %d\n", queue[front]);
 		front = -1;
 		rear = -1;
 	}
 	else
 	{
-		num = cir_queue[front];
-		front = (front +1)%size;
-		printf("The deleted element is %d\n", num);
+		printf("Element deleted is %d\n", queue[front]);
+		front++;
 	}
 }
 
@@ -84,20 +79,12 @@ void display()
 {
 	int i;
 	if(front == -1)
+	    printf("queue is empty\n");
+	else
 	{
-		printf("queue is empty\n");
-	}
-	else if(front == rear)
-	    printf("%d", cir_queue[front]);
-	else if(front < rear)
-	{
+		printf("Queue is : \n");
 		for(i = front; i <= rear; i++)
-		    printf("%d ", cir_queue[i]);
-	}
-	else if(rear < front)
-	{
-		for(i = front; i != rear; i = (i+1)%size)
-		    printf("%d ", cir_queue[i]);
-		printf("%d\n", cir_queue[rear]);
+		    printf("%d ", queue[i]);
+	    printf("\n");
 	}
 }
